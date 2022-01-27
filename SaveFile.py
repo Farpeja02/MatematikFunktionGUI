409c40e7bf2a948194186db91d8141d535a7ef9e
Farpeja02 <74357609+Farpeja02@users.noreply.github.com>
2022-01-27 14:56:58 +0100

    Iteration 1.0: Bare bone setup of project with funktions that pass.

---
+++ SaveFile.py
@@ -0,0 +1,4 @@
+
+
+def Save():
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# importing tkinter module
+from tkinter import *
+from tkinter.ttk import *
+from MFfile import MF
+from SaveFile import Save
 
 
-# Press the green button in the gutter to run the script.
+class mainWindow:
+    def __init__(self):
+        # creating tkinter window
+        self.root = Tk()
+
+        velkomst = Label(self.root, text="Velkommen til fodboldtur GUI")
+        velkomst.pack(pady=10)
+
+
+        self.add = Entry(self.root)
+        self.add.pack()
+
+        self.runButton = Button(self.root, text="Run", command= MF())
+        self.runButton.pack(padx = 20, pady = 10,side=LEFT)
+
+        self.saveButton = Button(self.root, text="Save", command= Save())
+        self.saveButton.pack(padx = 20, pady = 10,side=LEFT)
+
+
+        # infinite loop
+        mainloop()
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main = mainWindow()
+
+    print("hej")
